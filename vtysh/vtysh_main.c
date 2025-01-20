27177e39f2ef48c610511199f3b75630072831fc
Fredi Raspall <fredi@githedgehog.com>
2025-01-20 16:37:36 +0100

    Add extensions for vtysh
    Add very simple support for vtysh extensions. The approach is
    similar to FRR's modules, but much simpler. FRR's modules cannot
    be used straight away since vtysh does not follow the same
    pattern as protocol daemons. E.g. it does not call frr_init().
    Extensions are loaded with --extension|-X in the cmd line and
    can be displayed with "show vtysh extensions". Extensions are just
    like modules (some dynamically-loadable library) with entry point
    "vtysh_extension". The code expects that the extension return 0
    on success and !=0 otherwise, but vtysh will start regardless.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- vtysh/vtysh_main.c
+++ vtysh/vtysh_main.c
@@ -282,6 +282,7 @@ struct option longopts[] = {
 	{"timestamp", no_argument, NULL, 't'},
 	{"no-fork", no_argument, NULL, OPTION_NOFORK},
 	{"exec-timeout", required_argument, NULL, OPTION_TIMEOUT},
+	{"extension", required_argument, NULL, 'X'},
 	{0}};
 
 bool vtysh_loop_exited;
@@ -394,6 +395,7 @@ void suid_off(void)
 	}
 }
 
+
 /* VTY shell main routine. */
 int main(int argc, char **argv, char **env)
 {
@@ -447,7 +449,7 @@ int main(int argc, char **argv, char **env)
 
 	/* Option handling. */
 	while (1) {
-		opt = getopt_long(argc, argv, "be:c:d:nf:H:mEhCwN:ut", longopts,
+		opt = getopt_long(argc, argv, "be:c:d:nf:H:mEhCwN:utX:", longopts,
 				  0);
 
 		if (opt == EOF)
@@ -531,6 +533,9 @@ int main(int argc, char **argv, char **env)
 				exit(1);
 			}
 			break;
+		case 'X':
+		    vtysh_register_extension(optarg);
+		    break;
 		default:
 			usage(1);
 			break;
@@ -578,6 +583,7 @@ int main(int argc, char **argv, char **env)
 	vtysh_config_init();
 
 	vty_init_vtysh();
+	vtysh_load_extensions();
 
 	if (!user_mode) {
 		/* Read vtysh configuration file before connecting to daemons.
@@ -826,6 +832,7 @@ int main(int argc, char **argv, char **env)
 	vtysh_rl_run();
 
 	vtysh_uninit();
+	vtysh_unload_extensions();
 
 	history_truncate_file(history_file, 1000);
 	printf("\n");
